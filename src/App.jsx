828c3eba9dccc5141fe917395db41e7b1e1f34b1
Anand kc <anandkc11111@gmail.com>
2025-02-10 12:21:20 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom"
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Contact from './Pages/Contact'
+import Menu from './Pages/Menu'
+import Services from './Pages/Services'
+import AllergyAdvice from './Pages/AllergyAdvice'
+import Login from "./Pages/Login"
+import Navigation from './components/Navigation'
+
+function App() {
+  return (
+    <div>
+      <Navigation/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/allergyadvice' element={<AllergyAdvice />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pages/AllergyAdvice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Advice() {
+  return (
+    <div>
+      Advice
+    </div>
+  )
+}
+
+export default Advice
